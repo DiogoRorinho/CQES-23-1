0f7dd969f04f2ba76fcac18f4cb270143c64ee38
DiogoRorinho <diogo_manuel_2@hotmail.com>
2026-04-29 21:59:30 +0100

    Update RelatorioController.cs

--- Relatorios/RelatorioController.cs
+++ Relatorios/RelatorioController.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System;
 using GestorEventosEsqueleto.Aplicacao;
 using GestorEventosEsqueleto.Partilhado;
 
@@ -7,6 +7,7 @@ namespace GestorEventosEsqueleto.Relatorios {
         private readonly AplicacaoController aplicacaoController;
         private readonly RelatorioView view;
         private readonly RelatorioModel model;
+        private bool regressarMenuPrincipal;
 
         public RelatorioController(AplicacaoController aplicacaoController, RelatorioView view, RelatorioModel model) {
             this.aplicacaoController = aplicacaoController;
@@ -15,19 +16,36 @@ namespace GestorEventosEsqueleto.Relatorios {
         }
 
         public void MostrarMenuModulo() {
-            view.MostrarMenuRelatorios();
+            regressarMenuPrincipal = false;
+
+            while (!regressarMenuPrincipal) {
+                try {
+                    view.MostrarMenuRelatorios();
+                    SelecionarOpcao(Console.ReadLine());
+                }
+                catch (Exception ex) {
+                    view.MostrarErroMenu(ex.Message);
+                }
+                finally {
+                    view.FinalizarOperacaoMenu();
+                }
+            }
         }
 
         public void SelecionarOpcao(string opcao) {
-            switch (opcao) {
+            switch (NormalizarOpcao(opcao)) {
+                case "1":
                 case "Listagem de inscritos por evento":
-                    view.MostrarListaEventos(model.ListarEventos());
+                    ApresentarRelatorioInscritosPorEvento();
                     break;
+                case "2":
                 case "Eventos com ocupacao":
                     ApresentarRelatorioEventosComOcupacao();
                     break;
+                case "0":
                 case "Regressar ao menu principal":
                     RegressarMenuPrincipal();
+                    regressarMenuPrincipal = true;
                     break;
                 default:
                     view.MostrarMensagem("Opcao de relatorios invalida.");
@@ -35,22 +53,47 @@ namespace GestorEventosEsqueleto.Relatorios {
             }
         }
 
+        public void ApresentarRelatorioInscritosPorEvento() {
+            view.MostrarListaEventos(model.ListarEventos());
+
+            view.SolicitarIdEvento();
+
+            int idEvento;
+            if (!int.TryParse(Console.ReadLine(), out idEvento)) {
+                view.MostrarMensagem("ID de evento invalido.");
+                return;
+            }
+
+            if (idEvento <= 0) {
+                view.MostrarMensagem("ID de evento invalido.");
+                return;
+            }
+
+            SelecionarEvento(idEvento);
+        }
+
         public void SelecionarEvento(int idEvento) {
             DadosRelatorio dadosRelatorio = model.ListarInscritosPorEvento(idEvento);
             DocumentoPdf relatorioPdf = model.ObterUltimoRelatorioGerado();
             view.ApresentarRelatorioEPdf(dadosRelatorio, relatorioPdf);
-            view.MostrarMenuRelatorios();
         }
 
         public void ApresentarRelatorioEventosComOcupacao() {
             DadosRelatorio dadosRelatorio = model.ListarEventosComOcupacao();
             DocumentoPdf relatorioPdf = model.ObterUltimoRelatorioGerado();
             view.ApresentarRelatorioEPdf(dadosRelatorio, relatorioPdf);
-            view.MostrarMenuRelatorios();
         }
 
         public void RegressarMenuPrincipal() {
             aplicacaoController.RegressarMenuPrincipal();
+        }
+
+        private string NormalizarOpcao(string opcao) {
+            if (string.IsNullOrWhiteSpace(opcao)) {
+                return string.Empty;
+            }
+
+            return opcao.Trim();
         }
     }
 }
